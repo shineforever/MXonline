d6f64a48f3d5f3b126dbd4ea8ad02e30cde1fa60
shineforever <shine_forever@yeah.net>
2017-01-31 20:25:27 +0800

    setting文件修改，支持中文，写入数据库时间为本地时间！

--- MXonline/settings.py
+++ MXonline/settings.py
@@ -44,8 +44,9 @@ INSTALLED_APPS = [
     'users',
     'courses',
     'organization',
-    'operation'
-
+    'operation',
+    'xadmin',
+    'crispy_forms'
 ]
 
 #重载user模块,APP名称+class
@@ -117,15 +118,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'  #中文支持，django1.8以后支持；1.8以前是zh-cn
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False   #默认是Ture，时间是utc时间，由于我们要用本地时间，所用手动修改为false！！！！
 
 
 # Static files (CSS, JavaScript, Images)
