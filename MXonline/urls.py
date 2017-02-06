51d293ddd0780a1485dbc1999631ff80275b3352
shineforever <shine_forever@yeah.net>
2017-02-06 22:46:33 +0800

    login 页面

--- MXonline/urls.py
+++ MXonline/urls.py
@@ -22,4 +22,5 @@ import xadmin
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
     url(r'^$', TemplateView.as_view(template_name="index.html"),name="index"),
+    url(r'^login/$', TemplateView.as_view(template_name="login.html"),name="login"),
 ]
