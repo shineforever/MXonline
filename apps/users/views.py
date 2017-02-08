03a85461504944dbc700e7826591f5c719223f6c
root <root@python27.(none)>
2017-02-08 18:25:29 +0800

    login view

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,24 @@
+# coding: utf-8
+
 from django.shortcuts import render
 
+from django.contrib.auth import authenticate,login
+
 # Create your views here.
+def user_login(request):
+    """
+    用户登录
+    :param request:
+    :return:
+    """
+    if request.method == 'POST':
+        user_name = request.POST.get('username','')
+        pass_word = request.POST.get('password','')
+        user = authenticate(username=user_name,password=pass_word)
+        if user is not None:
+            login(request,user)
+            return render(request,'index.html')
+        else:
+            return render(request,'login.html',{})
+    elif request.method == 'GET':
+        return render(request,'index.html')
