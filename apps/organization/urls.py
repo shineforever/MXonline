74166e9b9cd28006048e8ff5eb5207f898cc2425
root <root@python27.(none)>
2017-03-09 14:55:51 +0800

    首页完善

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     url(r'^home/(?P<org_id>\d+)/$', OrgHomeView.as_view(),name="org_home"),
     url(r'^course/(?P<course_id>\d+)/$', OrgCourseView.as_view(),name="org_course"),
     url(r'^desc/(?P<course_id>\d+)/$', OrgDescView.as_view(),name="org_desc"),
-    url(r'^teacher/(?P<course_id>\d+)/$', OrgTeacherView.as_view(),name="org_teacher"),
+    url(r'^org_teacher/(?P<course_id>\d+)/$', OrgTeacherView.as_view(),name="org_teacher"),
     #机构收藏页面
     url(r'^add_fav/$', AddFavView.as_view(),name="add_fav"),
     #讲师列表页面
