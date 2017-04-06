802f7007dfb574b9f7623ff3fa99ce7de85c963d
shineforever <shine_forever@yeah.net>
2017-04-06 10:32:20 +0800

    首页功能完善

--- apps/organization/models.py
+++ apps/organization/models.py
@@ -27,6 +27,7 @@ class CourseOrg(models.Model):
     """
     name = models.CharField(max_length=50,verbose_name=u'机构名称')
     desc = models.TextField(verbose_name=u'机构描述')
+    tag = models.CharField(max_length=10,default=u'全国知名',verbose_name=u'标签')
     category = models.CharField(default="pxjg",max_length=20,verbose_name=u"机构类别",choices=(("pxjg","培训机构"),("gx","高校"),("gr","个人")))
     click_nums = models.IntegerField(default=0,verbose_name=u'点击数')
     fav_nums = models.IntegerField(default=0,verbose_name=u'收藏数')
--- apps/users/views.py
+++ apps/users/views.py
@@ -75,7 +75,7 @@ class LoginView(View):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return render(request, 'index.html')
+                    return HttpResponseRedirect(reverse('index'))
                 else:
                     return render(request, 'login.html', {"msg": "用户未激活！"})
             else:
